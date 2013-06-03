01fad0f542b8a7cd65abaab65fcd2729e84ce48b
Ale Almuna <ale@almuna.cl>
2013-06-03 10:54:54 -0400

    Move sampling code to a method
    take_sample send the sampling instruction to the arduino and then post
    the values to each device.

--- sensaClient.py
+++ sensaClient.py
@@ -35,7 +35,8 @@ except serial.SerialException:
     logging.error('Device can not be found or can not be configured')
 time.sleep(2)
 
-while True:
+# read values from serial port and posts it to API
+def take_sample():
     ardu.write('1')
     line = ardu.readline()
     humi, temp, moist = line.split(',')
@@ -43,15 +44,21 @@ while True:
     temp = {'value': int(float(temp.strip()))}
     moist = {'value': float(moist.strip())}
     try:
-        requests.post(humi_feed, data=json.dumps(humi), headers=headers)
+        r = requests.post(humi_feed, data=json.dumps(humi), headers=headers)
+	      if (r.status_code != 201): logging.error(r.text)
     except requests.exceptions.RequestException:
         network.restart()
     try:
-        requests.post(temp_feed, data=json.dumps(temp), headers=headers)
+        r = requests.post(temp_feed, data=json.dumps(temp), headers=headers)
+	      if (r.status_code != 201): logging.error(r.text)
     except requests.exceptions.RequestException:
         network.restart()
     try:
-        requests.post(moist_feed, data=json.dumps(moist), headers=headers)
+        r = requests.post(moist_feed, data=json.dumps(moist), headers=headers)
     except requests.exceptions.RequestException:
+	      if (r.status_code != 201): logging.error(r.text)
         network.restart()
+
+while True:
+    take_sample()
     time.sleep(delay)
